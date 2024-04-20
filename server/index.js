e45bcaf99a24a8029c132705c7b87b5b36964da6
Rahulrockss <rahulskumar343@gmail.com>
2024-04-20 21:59:42 +0530

    generateFile for Coding

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 
+const { generateFile} = require('./generateFile.js')
+
 const app = express();
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.get("/",(req,res)=>{
@@ -11,10 +14,12 @@ app.listen(5000,()=>{
     console.log("Ap running")
 })
 
-app.post("/run",(req,res)=>{
+app.post("/run", async (req,res)=>{
     const {language="cpp", code}= req.body
     if(code===undefined){
         return res.status(400).json({success:false,error:"Code is Empty"})
     }
-    return res.json({language,code});
+
+    const filepath = await generateFile(language,code)
+    return res.json({filepath});
 })
