af5aa31dcf8ee054eed1ec0fe191ee5d38b438b8
Rahulrockss <rahulskumar343@gmail.com>
2024-04-20 21:26:35 +0530

    Postman Testing

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-const express = express();
+const express = require("express")
+
+const app = express();
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.json("hello")
+})
+
+app.listen(5000,()=>{
+    console.log("Ap running")
+})
+
+app.post("/run",(req,res)=>{
+    const {language="cpp", code}= req.body
+    if(code===undefined){
+        return res.status(400).json({success:false,error:"Code is Empty"})
+    }
+    return res.json({language,code});
+})
